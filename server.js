3ad12620236e1e031f1979bb9770e99727215c20
EC2 Default User <ec2-user@ip-172-31-38-42.us-east-2.compute.internal>
2019-12-04 01:24:06 +0000

    updated pages, added vue

--- server.js
+++ server.js
@@ -16,4 +16,90 @@ mongoose.connect('mongodb://localhost:27017/tacoTuesday', {
     useNewUrlParser: true
 });
 
+/*-------------------ATTENDEES-------------------------*/
+
+const attendeeSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    partySize: { type: Number, default: 0 },
+    phone: String,
+    message: String,
+});
+
+const Attendee = mongoose.model('attendee', attendeeSchema);
+
+// Add a new attendee to the database
+app.post("/api/attendees", async(req, res) => {
+    const attendee = new Attendee({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        partySize: req.body.partySize,
+        phone: req.body.phone,
+        message: req.body.message
+    });
+    try {
+        console.log("attendee posted: ", attendee);
+        await attendee.save();
+        res.send(attendee);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+
+// Get a list of all the attendees
+app.get('/api/attendees', async(req, res) => {
+    try {
+        let attendees = await Attendee.find();
+        res.send(attendees);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+/*-------------------REVIEWS-------------------------*/
+
+const reviewerSchema = new mongoose.Schema({
+    review: String,
+    firstName: String,
+    lastName: String,
+});
+
+const Review = mongoose.model('reviewer', reviewerSchema);
+
+// Add a new review to the database
+app.post("/api/reviews", async(req, res) => {
+    const review = new Review({
+        review: req.body.review,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    });
+    try {
+        console.log("review posted: ", review);
+        await review.save();
+        res.send(review);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+
+// Get a list of all the reviews
+app.get('/api/reviews', async(req, res) => {
+    try {
+        let reviews = await Review.find();
+        res.send(reviews);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
 app.listen(3005, () => console.log('Server listening on port 3005!'));
