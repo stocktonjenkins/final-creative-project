61a3cce5683fa8bcb7723b46751413ae85d146cf
EC2 Default User <ec2-user@ip-172-31-38-42.us-east-2.compute.internal>
2019-12-05 17:25:38 +0000

    finished review/reserve

--- public/js/reserve-script.js
+++ public/js/reserve-script.js
@@ -13,12 +13,19 @@ new Vue({
             message: ''    
         },
         attendees: [],
+        totalAttendance: 0,
     },
     methods: {
         async submitRSVP() {
+            if(this.isNullOrEmpty(this.attendee.firstName) ||
+               this.isNullOrEmpty(this.attendee.lastName) ||
+               this.isNullOrEmpty(this.attendee.sizeOfParty)) {
+                   this.failedSubmission();
+                   return false;
+               }
             console.log('attendee: ', this.attendee);
             try {
-                let resposne = await axios.post("/api/attendees", {
+                let response = await axios.post("/api/attendees", {
                     firstName: this.attendee.firstName,
                     lastName: this.attendee.lastName,
                     partySize: this.attendee.sizeOfParty,
@@ -39,15 +46,28 @@ new Vue({
             swal("Success!", "We'll see you at Taco Tuesday, " +  firstName + "! 🌮", "success");
             document.getElementById('taco-form').reset();
         },
+        failedSubmission() {
+            swal("Submission Incomplete!", "Your form was not complete. Please try again.", "warning");
+            document.getElementById('taco-form').reset();
+        },
         async getAttendees() {
             try {
                 let response = await axios.get("/api/attendees");
                 this.attendees = response.data;
+                this.setTotalAttendance();
                 console.log("attendees: ", this.attendees);
             } catch (error) {
                 console.log(error);
             }
         },
+        setTotalAttendance() {
+            this.attendees.forEach(attendee => {
+               this.totalAttendance += attendee.partySize; 
+            });
+        },
+        isNullOrEmpty(input) {
+            return input == "" || input == null;
+        }
     },
     created() {
         this.getAttendees();
--- public/js/review.js
+++ public/js/review.js
@@ -14,6 +14,12 @@ new Vue({
     },
     methods: {
         async submitReview() {
+            if(this.isNullOrEmpty(this.reviewer.review) ||
+               this.isNullOrEmpty(this.reviewer.firstName) ||
+               this.isNullOrEmpty(this.reviewer.lastName)) {
+                   this.failedSubmission();
+                   return false;
+               }
             console.log('review: ', this.reviewer);
             try {
                 let response = await axios.post("/api/reviews", {
@@ -30,18 +36,25 @@ new Vue({
             }
         },
         submittedSuccessful(firstName) {
-            swal("Thank you, " +  firstName + " for your Review!");
+            swal("Submission Complete!", "Thank you, " +  firstName + ", for your Review!", "success");
+            document.getElementById('review-form').reset();
+        },
+        failedSubmission() {
+            swal("Submission Incomplete!", "Your form was not complete. Please try again.", "warning");
             document.getElementById('review-form').reset();
         },
         async getReviews() {
             try {
                 let response = await axios.get("/api/reviews");
                 this.reviews = response.data;
-                console.log("attendees: ", this.attendees);
+                console.log("reviews: ", this.reviews);
             } catch (error) {
                 console.log(error);
             }
         },
+        isNullOrEmpty(input) {
+            return input == "" || input == null;
+        }
     },
     created() {
         this.getReviews();
